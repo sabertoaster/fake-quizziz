96049338d0bf3ea68c9efe19386bd351a0c0b7fa
sabertoaster <phidaotram123@gmail.com>
2021-11-08 11:06:31 +0700

    Second commit (got the bar and top of UI present)

--- main.js
+++ main.js
@@ -3,12 +3,20 @@ function runBar(){
 	setInterval( function (){
 		if (x!=0) {
 				x-=.1;
-				document.getElementById("bar").innerHTML = x.toFixed(0) + "%";
 				document.getElementById("bar").style.width = x + "%";
 			}
 		if (x==0) {
 			document.getElementById("bar").innerHTML = "";
 		}
-
+		if (x>50){
+			document.getElementById("bar").style.backgroundColor = "green";
+		}
+		if (x<50 && x>25){
+			document.getElementById("bar").style.backgroundColor = "yellow";
+		}
+		if (x<25){
+			document.getElementById("bar").style.backgroundColor = "red";
+		}
 	}, 10);
+	document.getElementById("bar").style.transition = ".5s";
 }
