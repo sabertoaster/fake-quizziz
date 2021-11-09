e417eb36609a40dfb098e1f0d8e3f5e1ba527d50
sabertoaster <phidaotram123@gmail.com>
2021-11-09 16:16:21 +0700

    complete 100% ( bar working properly, animation fluent )

--- main.js
+++ main.js
@@ -1,40 +1,93 @@
-var x=100;
+var bar=100;
 var time=10;
-var i=time/100*2;
-var questionPack = ["1+1=?","2+2=?","3+3=?",""];
+var speed=time/100*1;
+var questionPack = ["10+10=?","20+20=?","30+30=?",""];
+var answerPack = [
+	[17,15,20,10],
+	[42,40,33,53],
+	[36,26,16,60],
+]
+var key = [	20,	40,	60,]
 var questionNumber = 0;
+var correct = 0;
 function onLoad(){
 	changeQuestion();
 	runBar();
 }
 
 function runBar(){
+	clearInterval(myTimer);
 	var myTimer = setInterval( function (){
-			x-=i;
-			document.getElementById("bar").style.width = x + "%";
-		if (x>50){
+			bar-=speed;
+			document.getElementById("bar").style.width = bar + "%";
+		if (bar>50){
 			document.getElementById("bar").style.backgroundColor = "green";
 		} 
-		if (x<50 && x>25){
+		if (bar<50 && bar>25){
 			document.getElementById("bar").style.backgroundColor = "yellow";
 		} 
-		if (x<25){
+		if (bar<25 && bar.toFixed(0)>=0){
 			document.getElementById("bar").style.backgroundColor = "red";
 		}
-		if (x.toFixed(0)<0){
-			alert("Hết giờ");
-			clearInterval(myTimer);
+		if (bar.toFixed(0)<0){
+			document.getElementById("bar").style.backgroundColor = "black";
+			//alert("Hết giờ");
 		}
-		console.log(x);
 	}, time);
 	document.getElementById("bar").style.transition = "background-color 1s";
 }
+
 function changeQuestion(){
 	document.getElementById("question").innerHTML = questionPack[questionNumber];
-	questionNumber += 1;
+	for (let i=0;i<4;i++){
+		let string="answer"+i;
+		document.getElementById(string).innerHTML = answerPack[questionNumber][i];
+	}
 }
-function checkAnswer(){
-	document.getElementById("question").innerHTML = questionPack[questionNumber];
-	questionNumber += 1;
-	x=100;	
+
+function transit(){
+	
+	let i;
+	let html = document.querySelectorAll("body *");
+	let body = document.querySelector("body");
+	//transit from question to notif
+	for (i = 0;i<html.length;i++) {
+		html[i].style.transition = 'opacity 0.5s';
+		html[i].style.opacity = "0";
+	}
+	body.style.backgroundSize = 'cover';
+	if (correct == 1) {
+		body.style.backgroundImage = 'url("Right.png")';
+	} else body.style.backgroundImage = 'url("Wrong.png")';
+	body.style.transition = 'opacity 0s';
+	body.style.opacity = "1";
+
+	//transit from notif to question 
+	setTimeout(function () {
+	body.style.backgroundSize = '';
+	body.style.backgroundImage = 'none';
+	for (i = 0;i<html.length;i++) {
+		html[i].style.transition = 'opacity 0.5s';
+		html[i].style.opacity = "1";
+	}
+	},1000);
+}
+function checkStreak(){
+
+}
+function checkAnswer(id){
+	let i;
+	// bug
+	if (key[questionNumber].toString()==document.getElementById(id).innerHTML)
+		{
+			correct = 1;
+		} else correct = 0;
+	console.log(correct);
+	setTimeout(function (){
+		questionNumber += 1;
+		changeQuestion();
+		document.getElementById("bar").style.transition = "background-color 0s";
+		bar=100;
+	},1000);
+	transit();
 }
